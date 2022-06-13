0ba282c14c6bc4c264857151a972359567d7c470
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2022-06-13 09:56:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function btnMenuAtivo() {
   const btnMenu = document.querySelector(".nav #btnMenu")
 
   function handleClick(e) {
+    if (e.type === "touchstart")
+      e.preventDefault
     const nav = document.querySelector(".nav")
     nav.classList.toggle("ativo")
     const ativo = nav.classList.contains("ativo")
@@ -15,6 +17,7 @@ function btnMenuAtivo() {
 
 
   btnMenu.addEventListener("click", handleClick)
+  btnMenu.addEventListener("touchstart", handleClick)
 }
 btnMenuAtivo()
 
