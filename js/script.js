93879c7baabfa149ebd5c461f1a8ea98a6d4004d
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2022-06-13 13:16:02 -0300

    .

--- js/script.js
+++ js/script.js
@@ -59,3 +59,35 @@ function smoothScroll() {
   })
 }
 smoothScroll()
+
+function slideAnimation() {
+  const sectionsLeftSlide = document.querySelectorAll(".js-leftSlide");
+  const sectionsRightSlide = document.querySelectorAll(".js-rightSlide");
+  const sectionsTop = window.innerHeight * 0.6;
+
+  function handleScroll() {
+    sectionsLeftSlide.forEach((itens) => {
+      const itensTop = itens.getBoundingClientRect().top
+      const sectionView = (itensTop - sectionsTop) < 0
+
+      if (sectionView) {
+        itens.classList.add("ativo")
+      }
+    })
+
+    sectionsRightSlide.forEach((itens) => {
+      const itensTop = itens.getBoundingClientRect().top
+      const sectionView = (itensTop - sectionsTop) < 0
+
+      if (sectionView) {
+        itens.classList.add("ativo")
+      }
+    })
+  }
+  handleScroll()
+
+  window.addEventListener("scroll", handleScroll)
+}
+slideAnimation()
+
+
