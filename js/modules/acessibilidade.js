c5e7a5ee59818142df6c693e9a9889ca0c1ffe78
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2022-07-01 13:36:43 -0300

    Update acessibilidade.js

--- js/modules/acessibilidade.js
+++ js/modules/acessibilidade.js
@@ -5,6 +5,7 @@ export default function initAcessibilidade() {
     const nav = document.querySelector(".nav ")
     const btnMenu = document.querySelector(".nav #btnMenu")
     nav.classList.toggle("ativo")
+    document.body.classList.toggle("ativo")
     const ativo = nav.classList.contains("ativo")
     btnMenu.setAttribute("aria-expanded", ativo)
     if (ativo)
